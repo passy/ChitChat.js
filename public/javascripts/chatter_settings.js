5d7a6e079c902324a2b444e269ad68866fa75167
Kyle Dorman <ked65@cornell.edu>
2013-12-07 17:05:27 -0500

    changed some file names to chitchat

--- public/javascripts/chatter_settings.js
+++
@@ -1,8 +0,0 @@
-// require.js syntax
-
-define({
-  // node_server_url: "chatter-js.herokuapp.com", //"chatter-js.herokuapp.com", //url where you are running your node server
-  // web_app_url: "intense-taiga-9081.herokuapp.com"//"intense-taiga-9081.herokuapp.com"; //url where your normal web app is running
-  node_server_url: "localhost",
-  web_app_url: "localhost"
-});
